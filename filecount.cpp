6725c267d71c19df73940047a2494d0ecce75d63
Kivanovic <49471023+Kivanovic@users.noreply.github.com>
2019-09-16 09:45:47 -0300

    Update filecount.cpp

--- filecount.cpp
+++ filecount.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
 #include <fstream>
 #include <vector>
+#include <list>
 #include <map>
 #include <sstream>
 #include <string_view>
@@ -11,6 +11,53 @@ using namespace std;
 /*--------------------------------------------------------*/
 namespace globals {
     static map<string, int> cnt_words;
+    static vector<pair<int, string>> srt_words;
+    static string filepath;
+}
+
+
+/*--------------------------------------------------------*/
+static string get_filename(string& path) {
+    string fname;
+    auto delim = path.end()-1;
+    for (; *delim != '\\' || *delim != '/'; --delim);
+    copy(delim, path.end(), fname);
+    return fname;
+}
+
+
+/*--------------------------------------------------------*/
+template <typename T, typename S>
+static auto& sort_stats(T& words, S& srt_words) {
+    for (auto&[k, v] : words)
+        srt_words.emplace_back(v, std::move(k));
+    sort(srt_words.begin(), srt_words.end(), [](auto& a, auto& b) {
+        return (a.first > b.first);
+    });
+    return srt_words;
+}
+
+/*--------------------------------------------------------*/
+template <typename T>
+static void display_stats(const T& words) {
+    for (auto& w : words)
+        printf("%s => %d\n", w.second.c_str(), w.first);
+}
+
+
+/*--------------------------------------------------------*/
+template <typename T>
+static void save_stats(const T& words) {
+    string fname = get_filename(globals::filepath) + "_stats.txt";
+
+    ofstream out(fname);
+    if (!out.is_open()) {
+        puts("| Failed to write statistics!\a");
+        fflush(stdin);
+        getchar();}
+
+    for (auto& w : words)
+        out << w.second << " => " << w.first << "\n";
 }
 
 
@@ -60,11 +107,40 @@ static void parse_file(string_view path) {
             if (is_word(w))
                 globals::cnt_words[w]++;
     }
+
+    sort_stats(globals::cnt_words, globals::srt_words);
+}
+
+
+/*--------------------------------------------------------*/
+static void save_or_display_stats() {
+    while (true) {
+        printf("Save or display statistics?\n\n");
+        printf("1 - Save\n");
+        printf("2 - Display\n\n");
+        printf(">> ");
+        int rep;
+        if (scanf(" %d", &rep) != 1) {
+            printf("| Inavalid option!\a\n");
+            continue;}
+        if (rep == 1) save_stats(globals::srt_words);
+        else if (rep == 2) display_stats(globals::srt_words);
+    }
 }
 
 
 /*--------------------------------------------------------*/
 int main(int argc, char** argv) {
-    parse_file("C:\\Users\\User\\Desktop\\Programmig\\C_Cpp_Lessons\\main.cpp");
+    if (argc != 2) {
+        puts("Invalid command!");
+        puts("Usage: fc filepath");
+        fflush(stdin);
+        getchar();
+    }
+
+    globals::filepath = argv[1];
+
+    parse_file(globals::filepath);
+    save_or_display_stats();
     return 0;
 }
